c4228fc3e1fd787e05bc1691afb7b06074dfe6a6
Diego N. <diegoleonsmr0@gmail.com>
2026-04-13 11:28:15 -0500

    Actualizar Allegro y refactorizar

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC = gcc
 PKG_CONFIG = pkg-config
-ALLEGRO_CFLAGS := $(shell $(PKG_CONFIG) --cflags allegro-5 allegro_image-5 allegro_dialog-5)
-ALLEGRO_LIBS := $(shell $(PKG_CONFIG) --libs allegro-5 allegro_image-5 allegro_dialog-5)
+ALLEGRO_CFLAGS := $(shell $(PKG_CONFIG) --cflags allegro-5 allegro_image-5 allegro_dialog-5 allegro_primitives-5)
+ALLEGRO_LIBS := $(shell $(PKG_CONFIG) --libs allegro-5 allegro_image-5 allegro_dialog-5 allegro_primitives-5)
 CFLAGS = -Wall -Wextra -std=c99 $(ALLEGRO_CFLAGS)
 LDFLAGS = $(ALLEGRO_LIBS) -lm
 SRC = main.c
--- main.c
+++ main.c
@@ -2,12 +2,36 @@
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_native_dialog.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define MAX_FILAS 13
 #define MAX_COLS 15
 #define WINDOW_WIDTH 720
 #define WINDOW_HEIGHT 624
 #define TILE_SIZE 48
+#define VELOCIDAD 2
+#define MAX_BOMBAS 5
+#define TIEMPO_EXPLOSION 2000
+#define RADIO_EXPLOSION 1
+
+typedef struct {
+    bool activa;
+    int x;
+    int y;
+    unsigned int tiempo_inicio;
+    bool explotada;
+} Bomba;
+
+typedef struct {
+    int posX;
+    int posY;
+    int direccion;  // 0=quieto, 1=izq, 2=der, 3=arriba, 4=abajo
+    int frame_animacion;
+    bool moviendose;
+    int radio_bomba;
+    int max_bombas;
+    int bombas_colocadas;
+} Jugador;
 
 static ALLEGRO_DISPLAY *pantalla = NULL;
 static ALLEGRO_TIMER *temporizador = NULL;
@@ -17,30 +41,157 @@ static ALLEGRO_BITMAP *bomberizq[3] = { NULL };
 static ALLEGRO_BITMAP *bomberar[3] = { NULL };
 static ALLEGRO_BITMAP *bomberder[3] = { NULL };
 static ALLEGRO_BITMAP *bomberab[3] = { NULL };
+static ALLEGRO_BITMAP *sprite_bomba = NULL;
+static ALLEGRO_BITMAP *sprite_explosion = NULL;
 
-static int posX = TILE_SIZE;
-static int posY = TILE_SIZE;
-static int direccion = 0;
+static Jugador jugador;
+static Bomba bombas[MAX_BOMBAS];
 static bool teclas[ALLEGRO_KEY_MAX] = { false };
 static bool redibujar = true;
 static bool ejecutando = true;
 static unsigned int milisegundos = 0;
 
-const char mapa[MAX_FILAS][MAX_COLS + 1] = {
+// Mapa con destructibles ('O')
+char mapa[MAX_FILAS][MAX_COLS + 1] = {
     "XXXXXXXXXXXXXXX",
-    "X             X",
-    "X X X X X X X X",
-    "X             X",
-    "X X X X X X X X",
-    "X             X",
-    "X X X X X X X X",
-    "X             X",
-    "X X X X X X X X",
-    "X             X",
-    "X X X X X X X X",
-    "X             X",
+    "XO O O O O O OX",
+    "XOXOXOXOXOXOXOX",
+    "X O O O O O O X",
+    "XOXOXOXOXOXOXOX",
+    "X O O O O O O X",
+    "XOXOXOXOXOXOXOX",
+    "X O O O O O O X",
+    "XOXOXOXOXOXOXOX",
+    "X O O O O O O X",
+    "XOXOXOXOXOXOXOX",
+    "XO O O O O O OX",
     "XXXXXXXXXXXXXXX"
 };
+
+static void inicializar_jugador(void) {
+    jugador.posX = TILE_SIZE;
+    jugador.posY = TILE_SIZE;
+    jugador.direccion = 4;
+    jugador.frame_animacion = 1;
+    jugador.moviendose = false;
+    jugador.radio_bomba = RADIO_EXPLOSION;
+    jugador.max_bombas = 1;
+    jugador.bombas_colocadas = 0;
+}
+
+static void inicializar_bombas(void) {
+    for (int i = 0; i < MAX_BOMBAS; i++) {
+        bombas[i].activa = false;
+        bombas[i].x = 0;
+        bombas[i].y = 0;
+        bombas[i].tiempo_inicio = 0;
+        bombas[i].explotada = false;
+    }
+}
+
+static bool hay_colision(int x, int y) {
+    if (x < 0 || x > WINDOW_WIDTH - TILE_SIZE || 
+        y < 0 || y > WINDOW_HEIGHT - TILE_SIZE) {
+        return true;
+    }
+    
+    int col_left = x / TILE_SIZE;
+    int col_right = (x + TILE_SIZE - 1) / TILE_SIZE;
+    int row_top = y / TILE_SIZE;
+    int row_bottom = (y + TILE_SIZE - 1) / TILE_SIZE;
+    
+    if (mapa[row_top][col_left] == 'X' || mapa[row_top][col_left] == 'O' ||
+        mapa[row_top][col_right] == 'X' || mapa[row_top][col_right] == 'O' ||
+        mapa[row_bottom][col_left] == 'X' || mapa[row_bottom][col_left] == 'O' ||
+        mapa[row_bottom][col_right] == 'X' || mapa[row_bottom][col_right] == 'O') {
+        return true;
+    }
+    
+    for (int i = 0; i < MAX_BOMBAS; i++) {
+        if (bombas[i].activa && !bombas[i].explotada) {
+            int bomba_col = bombas[i].x / TILE_SIZE;
+            int bomba_row = bombas[i].y / TILE_SIZE;
+            
+            int jugador_col_central = (x + TILE_SIZE/2) / TILE_SIZE;
+            int jugador_row_central = (y + TILE_SIZE/2) / TILE_SIZE;
+            
+            if (jugador_col_central == bomba_col && jugador_row_central == bomba_row) {
+                if (jugador_col_central != (jugador.posX + TILE_SIZE/2) / TILE_SIZE ||
+                    jugador_row_central != (jugador.posY + TILE_SIZE/2) / TILE_SIZE) {
+                    return true;
+                }
+            }
+        }
+    }
+    
+    return false;
+}
+
+static void colocar_bomba(void) {
+    if (jugador.bombas_colocadas >= jugador.max_bombas) return;
+    
+    int col = (jugador.posX + TILE_SIZE/2) / TILE_SIZE;
+    int row = (jugador.posY + TILE_SIZE/2) / TILE_SIZE;
+    
+    for (int i = 0; i < MAX_BOMBAS; i++) {
+        if (bombas[i].activa && !bombas[i].explotada) {
+            if (bombas[i].x / TILE_SIZE == col && bombas[i].y / TILE_SIZE == row) {
+                return;
+            }
+        }
+    }
+    
+    for (int i = 0; i < MAX_BOMBAS; i++) {
+        if (!bombas[i].activa) {
+            bombas[i].activa = true;
+            bombas[i].x = col * TILE_SIZE;
+            bombas[i].y = row * TILE_SIZE;
+            bombas[i].tiempo_inicio = milisegundos;
+            bombas[i].explotada = false;
+            jugador.bombas_colocadas++;
+            break;
+        }
+    }
+}
+
+static void actualizar_bombas(void) {
+    for (int i = 0; i < MAX_BOMBAS; i++) {
+        if (bombas[i].activa && !bombas[i].explotada) {
+            if (milisegundos - bombas[i].tiempo_inicio >= TIEMPO_EXPLOSION) {
+                bombas[i].explotada = true;
+                
+                int centro_col = bombas[i].x / TILE_SIZE;
+                int centro_row = bombas[i].y / TILE_SIZE;
+                
+                for (int r = 0; r <= jugador.radio_bomba; r++) {
+                    if (centro_row - r >= 0) {
+                        if (mapa[centro_row - r][centro_col] == 'O') {
+                            mapa[centro_row - r][centro_col] = ' ';
+                        }
+                    }
+                    if (centro_row + r < MAX_FILAS) {
+                        if (mapa[centro_row + r][centro_col] == 'O') {
+                            mapa[centro_row + r][centro_col] = ' ';
+                        }
+                    }
+                    if (centro_col - r >= 0) {
+                        if (mapa[centro_row][centro_col - r] == 'O') {
+                            mapa[centro_row][centro_col - r] = ' ';
+                        }
+                    }
+                    if (centro_col + r < MAX_COLS) {
+                        if (mapa[centro_row][centro_col + r] == 'O') {
+                            mapa[centro_row][centro_col + r] = ' ';
+                        }
+                    }
+                }
+                
+                bombas[i].activa = false;
+                jugador.bombas_colocadas--;
+            }
+        }
+    }
+}
 
 static void dibujar_mapa(void) {
     for (int row = 0; row < MAX_FILAS; row++) {
@@ -56,44 +207,124 @@ static void dibujar_mapa(void) {
     }
 }
 
+static void dibujar_bombas(void) {
+    for (int i = 0; i < MAX_BOMBAS; i++) {
+        if (bombas[i].activa) {
+            unsigned int tiempo_restante = TIEMPO_EXPLOSION - (milisegundos - bombas[i].tiempo_inicio);
+            bool visible = true;
+            
+            if (tiempo_restante < 500) {
+                visible = (milisegundos / 50) % 2 == 0;
+            } else if (tiempo_restante < 1000) {
+                visible = (milisegundos / 100) % 2 == 0;
+            }
+            
+            if (visible && sprite_bomba) {
+                al_draw_bitmap(sprite_bomba, bombas[i].x, bombas[i].y, 0);
+            } else if (visible) {
+                // Dibujar un círculo temporal si no hay sprite
+                al_draw_filled_circle(bombas[i].x + TILE_SIZE/2, bombas[i].y + TILE_SIZE/2, 
+                                    TILE_SIZE/3, al_map_rgb(0, 0, 0));
+            }
+        }
+    }
+}
+
+static void dibujar_personaje(void) {
+    if (jugador.moviendose) {
+        jugador.frame_animacion = (milisegundos / 100) % 3;
+    } else {
+        jugador.frame_animacion = 1;  // Frame idle
+    }
+    
+    ALLEGRO_BITMAP *sprite = bomberab[1];  // Default mirando abajo idle
+
+    switch (jugador.direccion) {
+    case 1:
+        sprite = bomberizq[jugador.frame_animacion];
+        break;
+    case 2:
+        sprite = bomberder[jugador.frame_animacion];
+        break;
+    case 3:
+        sprite = bomberar[jugador.frame_animacion];
+        break;
+    case 4:
+        sprite = bomberab[jugador.frame_animacion];
+        break;
+    default:
+        switch (jugador.direccion) {
+        case 1: sprite = bomberizq[1]; break;
+        case 2: sprite = bomberder[1]; break;
+        case 3: sprite = bomberar[1]; break;
+        case 4: sprite = bomberab[1]; break;
+        }
+        break;
+    }
+
+    al_draw_bitmap(sprite, jugador.posX, jugador.posY, 0);
+}
+
+static void actualizar_movimiento(void) {
+    jugador.moviendose = false;
+    
+    int prev_x = jugador.posX;
+    int prev_y = jugador.posY;
+    
+    if (teclas[ALLEGRO_KEY_LEFT]) {
+        jugador.direccion = 1;
+        jugador.moviendose = true;
+        jugador.posX -= VELOCIDAD;
+        if (hay_colision(jugador.posX, jugador.posY)) {
+            jugador.posX = prev_x;
+        }
+    }
+    else if (teclas[ALLEGRO_KEY_RIGHT]) {
+        jugador.direccion = 2;
+        jugador.moviendose = true;
+        jugador.posX += VELOCIDAD;
+        if (hay_colision(jugador.posX, jugador.posY)) {
+            jugador.posX = prev_x;
+        }
+    }
+    
+    if (teclas[ALLEGRO_KEY_UP]) {
+        jugador.direccion = 3;
+        jugador.moviendose = true;
+        jugador.posY -= VELOCIDAD;
+        if (hay_colision(jugador.posX, jugador.posY)) {
+            jugador.posY = prev_y;
+        }
+    }
+    else if (teclas[ALLEGRO_KEY_DOWN]) {
+        jugador.direccion = 4;
+        jugador.moviendose = true;
+        jugador.posY += VELOCIDAD;
+        if (hay_colision(jugador.posX, jugador.posY)) {
+            jugador.posY = prev_y;
+        }
+    }
+    
+    if (!jugador.moviendose) {
+        jugador.direccion = 0;
+    }
+}
+
 static void limpiar(void) {
     for (int i = 0; i < 3; i++) {
-        if (terreno[i]) {
-            al_destroy_bitmap(terreno[i]);
-            terreno[i] = NULL;
-        }
-        if (bomberizq[i]) {
-            al_destroy_bitmap(bomberizq[i]);
-            bomberizq[i] = NULL;
-        }
-        if (bomberar[i]) {
-            al_destroy_bitmap(bomberar[i]);
-            bomberar[i] = NULL;
-        }
-        if (bomberder[i]) {
-            al_destroy_bitmap(bomberder[i]);
-            bomberder[i] = NULL;
-        }
-        if (bomberab[i]) {
-            al_destroy_bitmap(bomberab[i]);
-            bomberab[i] = NULL;
-        }
+        if (terreno[i]) al_destroy_bitmap(terreno[i]);
+        if (bomberizq[i]) al_destroy_bitmap(bomberizq[i]);
+        if (bomberar[i]) al_destroy_bitmap(bomberar[i]);
+        if (bomberder[i]) al_destroy_bitmap(bomberder[i]);
+        if (bomberab[i]) al_destroy_bitmap(bomberab[i]);
     }
+    
+    if (sprite_bomba) al_destroy_bitmap(sprite_bomba);
+    if (sprite_explosion) al_destroy_bitmap(sprite_explosion);
 
-    if (temporizador) {
-        al_destroy_timer(temporizador);
-        temporizador = NULL;
-    }
-
-    if (cola_eventos) {
-        al_destroy_event_queue(cola_eventos);
-        cola_eventos = NULL;
-    }
-
-    if (pantalla) {
-        al_destroy_display(pantalla);
-        pantalla = NULL;
-    }
+    if (temporizador) al_destroy_timer(temporizador);
+    if (cola_eventos) al_destroy_event_queue(cola_eventos);
+    if (pantalla) al_destroy_display(pantalla);
 
     al_shutdown_image_addon();
     al_shutdown_native_dialog_addon();
@@ -110,50 +341,27 @@ static ALLEGRO_BITMAP *cargar_sprite(const char *path) {
 }
 
 static void cargar_recursos(void) {
-    terreno[0] = cargar_sprite("Sprites/Terreno/block00.bmp");
-    terreno[1] = cargar_sprite("Sprites/Terreno/block01.bmp");
-    terreno[2] = cargar_sprite("Sprites/Terreno/block02.bmp");
-
+    terreno[0] = cargar_sprite("Sprites/Terreno/block00.bmp");  // Suelo
+    terreno[1] = cargar_sprite("Sprites/Terreno/block01.bmp");  // Destruible
+    terreno[2] = cargar_sprite("Sprites/Terreno/block02.bmp");  // Pared
+    
     bomberizq[0] = cargar_sprite("Sprites/Bomberman/izq00.bmp");
     bomberizq[1] = cargar_sprite("Sprites/Bomberman/izq01.bmp");
     bomberizq[2] = cargar_sprite("Sprites/Bomberman/izq02.bmp");
-
+    
     bomberar[0] = cargar_sprite("Sprites/Bomberman/ar00.bmp");
     bomberar[1] = cargar_sprite("Sprites/Bomberman/ar01.bmp");
     bomberar[2] = cargar_sprite("Sprites/Bomberman/ar02.bmp");
-
+    
     bomberder[0] = cargar_sprite("Sprites/Bomberman/der00.bmp");
     bomberder[1] = cargar_sprite("Sprites/Bomberman/der01.bmp");
     bomberder[2] = cargar_sprite("Sprites/Bomberman/der02.bmp");
-
+    
     bomberab[0] = cargar_sprite("Sprites/Bomberman/ab00.bmp");
     bomberab[1] = cargar_sprite("Sprites/Bomberman/ab01.bmp");
     bomberab[2] = cargar_sprite("Sprites/Bomberman/ab02.bmp");
-}
-
-static void dibujar_personaje(void) {
-    int frame = (milisegundos / 100) % 3;
-    ALLEGRO_BITMAP *sprite = bomberab[1];
-
-    switch (direccion) {
-    case 1:
-        sprite = bomberizq[frame];
-        break;
-    case 2:
-        sprite = bomberder[frame];
-        break;
-    case 3:
-        sprite = bomberar[frame];
-        break;
-    case 4:
-        sprite = bomberab[frame];
-        break;
-    default:
-        sprite = bomberab[1];
-        break;
-    }
-
-    al_draw_bitmap(sprite, posX, posY, 0);
+    
+    sprite_bomba = al_load_bitmap("Sprites/Terreno/bomba.bmp");
 }
 
 int main(void) {
@@ -162,6 +370,11 @@ int main(void) {
     }
 
     if (!al_init_image_addon()) {
+        return EXIT_FAILURE;
+    }
+
+    if (!al_init_primitives_addon()) {
+        fprintf(stderr, "Error al inicializar allegro_primitives\n");
         return EXIT_FAILURE;
     }
 
@@ -195,6 +408,8 @@ int main(void) {
     al_register_event_source(cola_eventos, al_get_keyboard_event_source());
     al_register_event_source(cola_eventos, al_get_timer_event_source(temporizador));
 
+    inicializar_jugador();
+    inicializar_bombas();
     cargar_recursos();
 
     al_start_timer(temporizador);
@@ -205,41 +420,38 @@ int main(void) {
 
         if (evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
             ejecutando = false;
-        } else if (evento.type == ALLEGRO_EVENT_KEY_DOWN) {
+        } 
+        else if (evento.type == ALLEGRO_EVENT_KEY_DOWN) {
             teclas[evento.keyboard.keycode] = true;
+            
             if (evento.keyboard.keycode == ALLEGRO_KEY_ESCAPE) {
                 ejecutando = false;
             }
-        } else if (evento.type == ALLEGRO_EVENT_KEY_UP) {
+            else if (evento.keyboard.keycode == ALLEGRO_KEY_SPACE) {
+                colocar_bomba();
+            }
+        } 
+        else if (evento.type == ALLEGRO_EVENT_KEY_UP) {
             teclas[evento.keyboard.keycode] = false;
-        } else if (evento.type == ALLEGRO_EVENT_TIMER) {
-            milisegundos += 16;
-            if (!teclas[ALLEGRO_KEY_LEFT] && !teclas[ALLEGRO_KEY_RIGHT] && !teclas[ALLEGRO_KEY_UP] && !teclas[ALLEGRO_KEY_DOWN]) {
-                direccion = 0;
-            }
-            if (teclas[ALLEGRO_KEY_LEFT]) {
-                direccion = 1;
-                posX -= 2;
-            }
-            if (teclas[ALLEGRO_KEY_RIGHT]) {
-                direccion = 2;
-                posX += 2;
-            }
-            if (teclas[ALLEGRO_KEY_UP]) {
-                direccion = 3;
-                posY -= 2;
-            }
-            if (teclas[ALLEGRO_KEY_DOWN]) {
-                direccion = 4;
-                posY += 2;
-            }
+        } 
+        else if (evento.type == ALLEGRO_EVENT_TIMER) {
+            milisegundos += 16;  // Aproximadamente 16ms por frame a 60fps
+            
+            actualizar_movimiento();
+            actualizar_bombas();
+            
             redibujar = true;
         }
 
         if (redibujar && al_is_event_queue_empty(cola_eventos)) {
             redibujar = false;
+            
+            al_clear_to_color(al_map_rgb(0, 0, 0));
+            
             dibujar_mapa();
+            dibujar_bombas();
             dibujar_personaje();
+            
             al_flip_display();
         }
     }
